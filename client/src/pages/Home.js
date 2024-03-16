ea888cfce08b39e7e41312e6dba918909e2f19df
Ranbir-GNE <ranbir2115113@gndec.ac.in>
2024-03-16 12:26:07 +0530

    Update Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -22,7 +22,7 @@ const Home = () => {
   useEffect(() => {
     const fetchUserData = async () => {
       try {
-        const url = 'http://localhost:5012/api/user/65ec332f92ffe03ab5dcfdf0';
+        const url = 'https://ed-connect-dashboard.vercel.app/api/user/65ec332f92ffe03ab5dcfdf0';
         console.log('Fetching user data from:', url);
         const response = await axios.get(url).then(
           (response)=>{
