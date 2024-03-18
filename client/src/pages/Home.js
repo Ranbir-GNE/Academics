1fd923e9f2c66b658e34644d16a535fae4722382
Sukhmangill977 <sukhmangill977@gmail.com>
2024-03-18 13:06:21 +0530

    push3

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,4 @@
-// Home.js
-
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Home.css';
 import ProgressChart from '../components/ProgressChart';
@@ -8,39 +6,30 @@ import Calendar from '../components/Calendar';
 import Schedule from '../components/Schedule';
 import AttendanceHistogram from '../components/AttendanceHistogram';
 import CoursePieChart from '../components/CoursePieChart';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
 import Download from '../components/Download';
-import Navbar from '../components/Navbar';
+import axios from 'axios';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBell, faVideo, faBullhorn } from '@fortawesome/free-solid-svg-icons';
 
 const Home = () => {
   const [userData, setUserData] = useState(null);
-
-  const [attendanceData, setAttendenceData] = useState()
-  // const [gradesData, setGradesData] = useState()
+  const [attendanceData, setAttendanceData] = useState([]);
 
   useEffect(() => {
     const fetchUserData = async () => {
       try {
         const url = 'https://edconnect-dashboard-backend.vercel.app/api/user/65ec332f92ffe03ab5dcfdf0';
         console.log('Fetching user data from:', url);
-        const response = await axios.get(url).then(
-          (response)=>{
-            setUserData(response.data);
-            setAttendenceData(response.data.Courses)
-          }
-        )
+        const response = await axios.get(url);
+        setUserData(response.data);
+        setAttendanceData(response.data.Courses);
       } catch (error) {
         console.error('Error fetching user data:', error);
       }
     };
-    
 
-    fetchUserData(); // Call the function to fetch user data
+    fetchUserData();
   }, []);
-
-  // Dummy data for grades hisGradesram
-
 
   return (
     <div className="home-container">
@@ -48,7 +37,6 @@ const Home = () => {
         <div className="welcome-container">
           <div className="welcome-box">
             <h2>Welcome to College Dashboard</h2>
-            <p>This is the home page of our college dashboard.</p>
           </div>
         </div>
         <div className="calendar-container">
@@ -62,17 +50,22 @@ const Home = () => {
         </div>
         <div className="schedule-container">
           <div className="schedule-box">
-            <h3>Today's Schedule</h3>
             <div className="schedule-item">
-              <h4>Class Activity Field</h4>
+              <h4>
+                <FontAwesomeIcon icon={faBullhorn} /> Quick updates from teacher
+              </h4>
               <p>Quick updates from teacher</p>
             </div>
             <div className="schedule-item">
-              <h4>Online Lecture Link</h4>
-              <p>Link to the online lecture</p>
+              <h4>
+                <FontAwesomeIcon icon={faVideo} /> Online Lecture Link
+              </h4>
+              <a href="https://edconnect-meeting.vercel.app/"> Lecture Link</a>
             </div>
             <div className="schedule-item">
-              <h4>Notifications</h4>
+              <h4>
+                <FontAwesomeIcon icon={faBell} /> Notifications
+              </h4>
               <p>Notifications from teacher or chatting app</p>
             </div>
           </div>
@@ -87,8 +80,8 @@ const Home = () => {
         </div>
         <div className="box">
           <Link to="/assignments">
-            <h3>Due Assignments</h3>
-            <p>Due assignment details here...</p>
+            <h3>Assignments</h3>
+            <p>Assignment details here...</p>
           </Link>
         </div>
         <div className="box">
@@ -97,7 +90,7 @@ const Home = () => {
         </div>
         <div className="box">
           <h3>Progress Chart</h3>
-          <ProgressChart  courses={attendanceData}/>
+          <ProgressChart courses={attendanceData} />
         </div>
       </div>
     </div>
