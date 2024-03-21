69d1db7acbacaa10dee87ce9b473902445639af7
Ranbir-GNE <ranbir2115113@gndec.ac.in>
2024-03-21 22:57:15 +0530

    changed Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Navbar.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 function Navbar() {
   return (
     <nav className="navbar">
       <ul>
+        <Link to="https://edconnect-dashboard-blond.vercel.app/">
+          <li>Dashboard</li>
+        </Link>
+
         <Link to="https://ed-connect.vercel.app">
           <li>College Portal</li>
         </Link>
@@ -13,56 +17,58 @@ function Navbar() {
         <Link to="https://event-calender-edconnect.vercel.app/">
           <li>Event Calender</li>
         </Link>
+        <Link to="/student">
+          <li>My Account</li>
+        </Link>
 
-        {/* <Link to="/admin">
-          <li>Admin</li>
-        </Link> */}
-
+        <a
+          href="https://edconnect-meeting.vercel.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <li>Online Session </li>
+        </a>
+        <Link to="https://chat-psi-jet.vercel.app/">
+          <li>Chat-App</li>
+        </Link>
         <Link to="https://ed-connect.vercel.app/student">
           <li>My Account</li>
         </Link>
-        
-        <a href="https://edconnect-meeting.vercel.app/" target="_blank" rel="noopener noreferrer">
+
+        <a
+          href="https://edconnect-meeting.vercel.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <li>Online Session </li>
         </a>
-        <a href="https://chat-psi-jet.vercel.app/" target="_blank" rel="noopener noreferrer">
-        <li>Chat-App</li>
-        </a>
 
-        {/* <Link to="/academics">
-          <li>Academics</li>
-        </Link> */}
-
-        {/* <Link to="/fees">
-          <li>Fees Collection</li>
-        </Link>
-
-        <Link to="/examination">
+        {/* <Link to="/examination">
           <li>Examinations</li>
         </Link>
 
         <Link to="/attendance">
           <li>Attendance</li>
-        </Link>
+        </Link> */}
 
-        <a href="https://curacare.vercel.app/" target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://curacare.vercel.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <li>Communicate</li>
-        </a> */}
+        </a>
 
         <Link to="https://ed-connect.vercel.app/settings">
           <li>Settings</li>
         </Link>
-
-        {/* <Link to="/change-password">
-          <li>Change Password</li>
-        </Link> */}
 
         <Link to="https://ed-connect.vercel.app/startpage">
           <li>Logout</li>
         </Link>
         {/* Add more links as needed */}
       </ul>
-    </nav >
+    </nav>
   );
 }
 
